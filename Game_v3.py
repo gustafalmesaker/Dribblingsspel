60b3e1257bfac3c89f59aab0e208835b1e206955
Oskar André <93657491+oskarandre@users.noreply.github.com>
2024-04-04 14:01:15 +0200

    Update Game_v3.py
    Added numbers for goals, and fixed fps problems. (disabled vsync)

--- Game_v3.py
+++ Game_v3.py
@@ -14,10 +14,11 @@ class cursor:
         self.x = x
         self.y = y
         self.radius = radius
-    
+
 class Game:
     def __init__(self):
         self.new_window = pyglet.window.Window(width=800, height=600)
+        self.new_window.set_vsync(False)
         self.fps_display = pyglet.window.FPSDisplay(self.new_window)
         self.cursor = cursor(x=self.new_window.width//2, y=self.new_window.height//2, radius=20)
        
@@ -32,14 +33,16 @@ class Game:
     
 
         self.new_window.push_handlers(self)
-        pyglet.clock.schedule_interval(self.update, 1/60.0)
+        pyglet.clock.schedule_interval(self.update, 1/10.0)
         self.new_window.set_mouse_visible(False)
     
-
     def draw(self):
+        goalIndex = 1
         for goal in self.goalCircles:
             pyglet.shapes.Circle(x=goal.x, y=goal.y, radius=goal.radius, color=goal.color).draw()
             pyglet.shapes.Circle(x=goal.x, y=goal.y, radius=goal.radius-5, color=(0, 0, 0)).draw()
+            pyglet.text.Label(str(goalIndex), font_name= 'Times New Roman', font_size=18, x=goal.x, y=goal.y).draw()
+            goalIndex += 1
         pyglet.shapes.Circle(x=self.cursor.x, y=self.cursor.y, radius=self.cursor.radius).draw()
 
     def on_draw(self):
@@ -52,13 +55,16 @@ class Game:
         self.cursor.x = x
         self.cursor.y = y
     
+    
     def update(self, dt):
         for goal in self.goalCircles:
             if (self.cursor.x - goal.x)**2 + (self.cursor.y - goal.y)**2 <= (goal.radius - self.cursor.radius)**2:
-                self.add_new_goal(goal)
+                
                 self.goalCircles.remove(goal)
-                self.goalCircles.append(goal)
+                self.add_new_goal(goal)
                 break
+
+        #print(len(self.goalCircles))
         
     def add_new_goal(self, goal):
         x = random.randint(50, self.new_window.width-50)
@@ -74,4 +80,4 @@ class Game:
     
 if __name__ == "__main__":
     game = Game()
-    pyglet.app.run()
+    pyglet.app.run(interval=1/120.0)
