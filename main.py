c67cd2baa849dc05d759ac84b7c087edf9203cb7
gustafalmesaker <114588941+gustafalmesaker@users.noreply.github.com>
2024-04-11 16:17:58 +0200

    model trained unsuccessfully

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from ultralytics import YOLO
 
 model = YOLO("yolov8n.yaml") #build the model
+#model = YOLO("yolov8n.pt") #build the model
 
 results = model.train(data="configs\config.yaml", epochs=1) # train the model
