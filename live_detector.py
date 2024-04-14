329da6e64cfd88e6291af6c4b7d7fc01b1d5eca7
Oskar André <93657491+oskarandre@users.noreply.github.com>
2024-04-14 21:18:34 +0200

    added new test video

--- live_detector.py
+++ live_detector.py
@@ -5,7 +5,7 @@ import cv2
 #model = YOLO("models\model_v0.pt") #load created model
 model = YOLO("yolov8n.pt") #load pretrained model
 
-video_path = 'videos\test_ball_detection.gif'
+video_path = "videos\Test_ball_detection_2.mp4"
 cap = cv2.VideoCapture(video_path)
 
 ret = True
