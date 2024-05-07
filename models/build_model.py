98b85bd88129f8854a989f656b6fdaabc6c77feb
gustafalmesaker <114588941+gustafalmesaker@users.noreply.github.com>
2024-05-07 15:48:25 +0200

    further refined model created with 20 epochs

--- models/build_model.py
+++ models/build_model.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
 
 #model = YOLO("yolov8n.yaml") #build the model
-model = YOLO("models\final_model.pt") #load pretrained model
+model = YOLO("models\\final_model.pt") #load pretrained model
 
-results = model.train(data="configs\config_only_ball.yaml", epochs=10) # train the model
+results = model.train(data="models\configs\config.yaml", epochs=20) # train the model
 
-results.save("final_ball_model.pt")
+model.save("final_model_20_epochs.pt")
